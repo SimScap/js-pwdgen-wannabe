c5baca5359819487e27b28c9c878cf0fe1c06708
SimoneScapin <simonescap87@gmail.com>
2022-02-21 15:28:35 +0100

    inizio

---
+++ js/main.js
@@ -0,0 +1,8 @@
+const firstName = prompt("Qual'è il tuo nome?")
+document.getElementById('name').innerHTML = firstName;
+
+const secondName = prompt("Qual'è il tuo cognome?")
+document.getElementById('surname').innerHTML = secondName;
+
+const favouriteColor = prompt("Qual'è il tuo colore preferito?")
+document.getElementById('color').innerHTML = favouriteColor;
